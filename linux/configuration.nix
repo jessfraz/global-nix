571c4f6d25c5b8937b37e0167e02aa568532bef2
Jess Frazelle <github@jessfraz.com>
2025-04-08 15:17:09 -0700

    base configuration
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- linux/configuration.nix
+++ linux/configuration.nix
@@ -1,16 +1,10 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }: {
-  imports = [];
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
   users.users.jessfraz = {
     isNormalUser = true;
-    description = "Jessie Frazelle";
   };
-
-  environment.systemPackages = [pkgs.git];
 }
