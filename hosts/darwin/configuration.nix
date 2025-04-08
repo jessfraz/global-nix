d651f86b8c7eacf38c6c2b253d33505dada7048a
Jess Frazelle <github@jessfraz.com>
2025-04-08 16:04:00 -0700

    macos settings
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- hosts/darwin/configuration.nix
+++ hosts/darwin/configuration.nix
@@ -4,7 +4,34 @@
   inputs,
   ...
 }: {
-  system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+  # macOS system preferences
+  system.defaults = {
+    NSGlobalDomain = {
+      # Show all the file extensions.
+      AppleShowAllExtensions = true;
+    };
+
+    CustomUserPreferences = {
+      "com.apple.finder" = {
+        # Show all files.
+        AppleShowAllFiles = true;
+        ShowPathBar = true;
+      };
+
+      "com.apple.dock" = {
+        autohide = true; # Auto-hide the Dock
+      };
+
+      "com.apple.menuextra.battery" = {
+        # Battery percentage in menu bar
+        ShowPercent = "YES";
+      };
+
+      "com.apple.menuextra.clock" = {
+        Show24Hour = true;
+      };
+    };
+  };
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
