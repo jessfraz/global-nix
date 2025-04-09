e2507c67f02bc7b37dee1dab6c1b71ff5fd27b23
Jess Frazelle <github@jessfraz.com>
2025-04-08 20:05:44 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -31,6 +31,17 @@
     # Load the other bash dotfiles we have.
     bashrcExtra = ''
       source ${config.home.homeDirectory}/.nixbash
+
+      function fetch-kc-token() {
+          export KITTYCAD_TOKEN=$(op --account kittycadinc.1password.com item get "KittyCAD Token" --fields credential --reveal)
+          export KITTYCAD_DEV_TOKEN=$(op --account kittycadinc.1password.com item get "KittyCAD Dev Token" --fields credential --reveal)
+      }
+
+      function vault-login() {
+          export VAULT_ADDR="http://vault.hawk-dinosaur.ts.net"
+          export GITHUB_VAULT_TOKEN=$(op --account kittycadinc.1password.com item get "GitHub Token Vault" --fields credential --reveal)
+          echo $GITHUB_VAULT_TOKEN | vault login -method=github token=-
+      }
     '';
 
     # Fix for https://github.com/nix-community/home-manager/issues/5997
