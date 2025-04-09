1a063c54f1ca21fd79960822a08e5bb7022cb646
Jess Frazelle <github@jessfraz.com>
2025-04-09 14:26:10 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- hosts/linux/configuration.nix
+++ hosts/linux/configuration.nix
@@ -28,11 +28,21 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      font-awesome
+      source-han-sans
+      source-han-sans-japanese
+      source-han-serif-japanese
+    ];
+    fontconfig.defaultFonts = {
+      serif = ["Noto Serif" "Source Han Serif"];
+      sansSerif = ["Noto Sans" "Source Han Sans"];
+    };
+  };
 
   services = {
     tailscale = {
