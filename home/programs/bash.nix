89fdbc796b97018c548a8864ff44ad720350e107
Jess Frazelle <github@jessfraz.com>
2025-04-08 20:12:59 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -33,13 +33,13 @@
       source ${config.home.homeDirectory}/.nixbash
 
       function fetch-kc-token() {
-          export KITTYCAD_TOKEN=$(op --account kittycadinc.1password.com item get "KittyCAD Token" --fields credential --reveal)
-          export KITTYCAD_DEV_TOKEN=$(op --account kittycadinc.1password.com item get "KittyCAD Dev Token" --fields credential --reveal)
+          export KITTYCAD_TOKEN=$(op --account kittycadinc.1password.com item get --vault Employee "KittyCAD Token" --fields credential --reveal)
+          export KITTYCAD_DEV_TOKEN=$(op --account kittycadinc.1password.com item get --vault Employee "KittyCAD Dev Token" --fields credential --reveal)
       }
 
       function vault-login() {
           export VAULT_ADDR="http://vault.hawk-dinosaur.ts.net"
-          export GITHUB_VAULT_TOKEN=$(op --account kittycadinc.1password.com item get "GitHub Token Vault" --fields credential --reveal)
+          export GITHUB_VAULT_TOKEN=$(op --account kittycadinc.1password.com item get --vault Employee "GitHub Token Vault" --fields credential --reveal)
           echo $GITHUB_VAULT_TOKEN | vault login -method=github token=-
       }
     '';
