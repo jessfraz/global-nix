6e98c503c68a101333dbcc368270082d5a36f0ee
Jess Frazelle <github@jessfraz.com>
2025-04-09 12:21:15 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -41,6 +41,15 @@
           export STRIPE_KEY=$(op --account kittycadinc.1password.com item get "stripe prod zoo" --fields credential --reveal)
       }
 
+      function fetch-hoops-key() {
+          export HOOPS_KEY=$(op --account kittycadinc.1password.com item get "Hoops Licence" --fields "new license key" --reveal)
+      }
+
+      function fetch-cockroach-license() {
+          export COCKROACHDB_ENTERPRISE_LICENSE=$(op --account kittycadinc.1password.com item get "CockroachDB Dev License" --fields "license key" --reveal)
+          export DATABASE_ROOT_CERT_PATH="${config.home.homeDirectory}/.cockroach/ca.crt"
+      }
+
       function vault-login() {
           export VAULT_ADDR="http://vault.hawk-dinosaur.ts.net"
           export GITHUB_VAULT_TOKEN=$(op --account kittycadinc.1password.com item get --vault Employee "GitHub Token Vault" --fields credential --reveal)
