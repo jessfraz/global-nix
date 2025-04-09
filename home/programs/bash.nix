3a2f96384eb628bd66c6466fbdb6f3112702ac90
Jess Frazelle <github@jessfraz.com>
2025-04-09 12:58:58 -0700

    cert
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -47,6 +47,10 @@
 
       function fetch-cockroach-license() {
           export COCKROACHDB_ENTERPRISE_LICENSE=$(op --account kittycadinc.1password.com item get "CockroachDB Dev License" --fields "license key" --reveal)
+          mkdir -p "${config.home.homeDirectory}/.cockroach"
+          echo "$(op --account kittycadinc.1password.com item get "CockroachDB Dev License" --fields "certificate" --reveal)" > "${config.home.homeDirectory}/.cockroach/ca.crt"
+          # Trim " from the file
+          sed -i 's/^"//;s/"$//' ${config.home.homeDirectory}/.cockroach/ca.crt
           export DATABASE_ROOT_CERT_PATH="${config.home.homeDirectory}/.cockroach/ca.crt"
       }
 
