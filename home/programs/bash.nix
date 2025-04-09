aac141b847aaf30fe859b1407ee6cd2637a63f8c
Jess Frazelle <github@jessfraz.com>
2025-04-08 17:12:13 -0700

    bash
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -1,7 +1,26 @@
 {
-  bash = {
+  programs.bash = {
     enable = true;
 
     enableCompletion = true;
+
+    shellOptions = [
+      # check the window size after each command and, if necessary,
+      # update the values of LINES and COLUMNS.
+      "checkwinsize"
+      # Case-insensitive globbing (used in pathname expansion)
+      "nocaseglob"
+      # Append to the Bash history file, rather than overwriting it
+      "histappend"
+      # Autocorrect typos in path names when using `cd`
+      "cdspell"
+      # `**/qux` will enter `./foo/bar/baz/qux`
+      "autocd"
+      # * Recursive globbing, e.g. `echo **/*.txt`
+      "extglob"
+      "globstar"
+      # Warn if closing shell with running jobs.
+      "checkjobs"
+    ];
   };
 }
