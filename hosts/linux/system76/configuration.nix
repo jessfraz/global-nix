3645f30565f8e634a1d089ad46a44143f0059c24
Jess Frazelle <github@jessfraz.com>
2025-04-10 12:44:53 -0700

    fix
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- hosts/linux/system76/configuration.nix
+++ hosts/linux/system76/configuration.nix
@@ -133,6 +133,41 @@
     };
   };
 
+  # Remove stupid gnome packages.
+  environment = {
+    gnome.excludePackages = with pkgs.gnome; [
+      baobab # disk usage analyzer
+      cheese # photo booth
+      eog # image viewer
+      epiphany # web browser
+      gedit # text editor
+      simple-scan # document scanner
+      totem # video player
+      yelp # help viewer
+      evince # document viewer
+      file-roller # archive manager
+      geary # email client
+      seahorse # password manager
+
+      # these should be self explanatory
+      gnome-calculator
+      gnome-calendar
+      gnome-characters
+      gnome-clocks
+      gnome-contacts
+      gnome-font-viewer
+      gnome-logs
+      gnome-maps
+      gnome-music
+      gnome-photos
+      gnome-screenshot
+      gnome-system-monitor
+      gnome-weather
+      gnome-disk-utility
+      pkgs.gnome-connections
+    ];
+  };
+
   security = {
     rtkit = {
       enable = true;
