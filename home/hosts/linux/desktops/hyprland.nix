0bb00fc9023342ae6df1bccd8b033b0d03c19f9e
Jess Frazelle <github@jessfraz.com>
2025-04-11 20:53:28 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/hosts/linux/desktops/hyprland.nix
+++ home/hosts/linux/desktops/hyprland.nix
@@ -67,96 +67,100 @@ in {
     };
   };
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = inputs.hyprland-plugins.packages.${pkgs.system}.hyprland;
+  wayland = {
+    windowManager = {
+      hyprland = {
+        enable = true;
+        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
 
-    plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
-    ];
+        plugins = [
+          inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus
+          inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+          inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
+        ];
 
-    xwayland = {
-      enable = true;
-    };
+        xwayland = {
+          enable = true;
+        };
 
-    systemd = {
-      enable = true;
-      variables = ["--all"];
-    };
+        systemd = {
+          enable = true;
+          variables = ["--all"];
+        };
 
-    settings = {
-      "$mod" = "SUPER";
-      "debug:disable_logs" = false;
-      "general:gaps_out" = 5;
-      #"decoration:inactive_opacity" = 0.8;
-      xwayland = {
-        force_zero_scaling = true;
+        settings = {
+          "$mod" = "SUPER";
+          "debug:disable_logs" = false;
+          "general:gaps_out" = 5;
+          #"decoration:inactive_opacity" = 0.8;
+          xwayland = {
+            force_zero_scaling = true;
+          };
+
+          input = {
+            kb_layout = "us";
+            natural_scroll = true;
+          };
+
+          cursor = {
+            no_hardware_cursors = true;
+          };
+
+          env = [
+            "GBM_BACKEND,nvidia-drm"
+            #"AQ_DRM_DEVICES,/dev/dri/by-path/pci-0000:01:00.0-card"
+          ];
+
+          exec-once = [
+            "${pkgs.mako}/bin/mako"
+            "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+            "${pkgs.waybar}/bin/waybar"
+          ];
+
+          layerrule = [];
+
+          bind =
+            [
+              "$mod, C, sendshortcut, CTRL, C, class:(google-chrome)"
+              "$mod, V, sendshortcut, CTRL, V, class:(google-chrome)"
+              "$mod, X, sendshortcut, CTRL, X, class:(google-chrome)"
+              "$mod, G, exec, ${pkgs.google-chrome}/bin/google-chrome-stable"
+              "$mod, Return, exec, gtk-launch com.mitchellh.ghostty.desktop"
+              "$mod, Left, movewindow, l"
+              "$mod, Right, movewindow, r"
+              "$mod SHIFT, Apostrophe, killactive,"
+              "$mod SHIFT, S, exec, ${inputs.hyprland-contrib.packages.${pkgs.system}.grimblast}/bin/grimblast --notify copy area"
+            ]
+            ++ (
+              # workspaces
+              # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+              builtins.concatLists (builtins.genList (
+                  x: let
+                    ws = let
+                      c = (x + 1) / 10;
+                    in
+                      builtins.toString (x + 1 - (c * 10));
+                  in [
+                    "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                    "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+                  ]
+                )
+                10)
+            );
+
+          bindle = [
+            ",XF86Launch4,   exec, ags -r 'recorder.start()'"
+            ",Print,         exec, ags -r 'recorder.screenshot()'"
+            "SHIFT,Print,    exec, ags -r 'recorder.screenshot(true)'"
+            ",XF86MonBrightnessUp,   exec, ags -r 'brightness.screen += 0.05; indicator.display()'"
+            ",XF86MonBrightnessDown, exec, ags -r 'brightness.screen -= 0.05; indicator.display()'"
+            ",XF86KbdBrightnessUp,   exec, ags -r 'brightness.kbd++; indicator.kbd()'"
+            ",XF86KbdBrightnessDown, exec, ags -r 'brightness.kbd--; indicator.kbd()'"
+            ",XF86AudioRaiseVolume,  exec, ags -r 'audio.speaker.volume += 0.05; indicator.speaker()'"
+            ",XF86AudioLowerVolume,  exec, ags -r 'audio.speaker.volume -= 0.05; indicator.speaker()'"
+          ];
+        };
       };
-
-      input = {
-        kb_layout = "us";
-        natural_scroll = true;
-      };
-
-      cursor = {
-        no_hardware_cursors = true;
-      };
-
-      env = [
-        "GBM_BACKEND,nvidia-drm"
-        #"AQ_DRM_DEVICES,/dev/dri/by-path/pci-0000:01:00.0-card"
-      ];
-
-      exec-once = [
-        "${pkgs.mako}/bin/mako"
-        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-        "${pkgs.waybar}/bin/waybar"
-      ];
-
-      layerrule = [];
-
-      bind =
-        [
-          "$mod, C, sendshortcut, CTRL, C, class:(google-chrome)"
-          "$mod, V, sendshortcut, CTRL, V, class:(google-chrome)"
-          "$mod, X, sendshortcut, CTRL, X, class:(google-chrome)"
-          "$mod, G, exec, ${pkgs.google-chrome}/bin/google-chrome-stable"
-          "$mod, Return, exec, gtk-launch com.mitchellh.ghostty.desktop"
-          "$mod, Left, movewindow, l"
-          "$mod, Right, movewindow, r"
-          "$mod SHIFT, Apostrophe, killactive,"
-          "$mod SHIFT, S, exec, ${inputs.hyprland-contrib.packages.${pkgs.system}.grimblast}/bin/grimblast --notify copy area"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-          builtins.concatLists (builtins.genList (
-              x: let
-                ws = let
-                  c = (x + 1) / 10;
-                in
-                  builtins.toString (x + 1 - (c * 10));
-              in [
-                "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-              ]
-            )
-            10)
-        );
-
-      bindle = [
-        ",XF86Launch4,   exec, ags -r 'recorder.start()'"
-        ",Print,         exec, ags -r 'recorder.screenshot()'"
-        "SHIFT,Print,    exec, ags -r 'recorder.screenshot(true)'"
-        ",XF86MonBrightnessUp,   exec, ags -r 'brightness.screen += 0.05; indicator.display()'"
-        ",XF86MonBrightnessDown, exec, ags -r 'brightness.screen -= 0.05; indicator.display()'"
-        ",XF86KbdBrightnessUp,   exec, ags -r 'brightness.kbd++; indicator.kbd()'"
-        ",XF86KbdBrightnessDown, exec, ags -r 'brightness.kbd--; indicator.kbd()'"
-        ",XF86AudioRaiseVolume,  exec, ags -r 'audio.speaker.volume += 0.05; indicator.speaker()'"
-        ",XF86AudioLowerVolume,  exec, ags -r 'audio.speaker.volume -= 0.05; indicator.speaker()'"
-      ];
     };
   };
 }
