5381b9cc52b530a9f375cee05b79ed70f1f0e33d
Jess Frazelle <github@jessfraz.com>
2025-04-30 13:32:17 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -69,6 +69,7 @@ in {
       function fetch-kc-token() {
           export KITTYCAD_TOKEN=$(op --account kittycadinc.1password.com item get --vault Employee "KittyCAD Token" --fields credential --reveal)
           export KITTYCAD_API_TOKEN=$KITTYCAD_TOKEN
+          export ZOO_TEST_TOKEN=$KITTYCAD_API_TOKEN
           export KITTYCAD_DEV_TOKEN=$(op --account kittycadinc.1password.com item get --vault Employee "KittyCAD Dev Token" --fields credential --reveal)
       }
       alias fetch-zoo-token="fetch-kc-token"
