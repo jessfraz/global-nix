fd2aae8bfe2d7e28f58c2f8ad8403f981df7ef39
Jess Frazelle <github@jessfraz.com>
2025-05-30 15:54:38 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -66,6 +66,10 @@ in {
           export ANTHROPIC_API_KEY=$(op --account my.1password.com item get "claude.ai" --fields apikey --reveal)
       }
 
+      function fetch-google-ai-key() {
+          export GOOGLE_API_KEY=$(op --account my.1password.com item get "Google AI Studio" --fields credential --reveal)
+      }
+
       function fetch-kc-token() {
           export KITTYCAD_TOKEN=$(op --account kittycadinc.1password.com item get --vault Employee "KittyCAD Token" --fields credential --reveal)
           export KITTYCAD_API_TOKEN=$KITTYCAD_TOKEN
