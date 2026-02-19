0d1933de73b07c0f271e85f13e982db077350fbc
Jessie Frazelle <github@jessfraz.com>
2026-02-18 19:04:14 -0800

    updates
    Signed-off-by: Jessie Frazelle <github@jessfraz.com>

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
     };
 
     codex = {
-      url = "git+https://github.com/openai/codex?ref=refs/tags/rust-v0.103.0&submodules=1";
+      url = "git+https://github.com/openai/codex?ref=refs/tags/rust-v0.104.0&submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
